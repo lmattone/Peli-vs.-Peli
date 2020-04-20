d216b806bce06e08f1c40600751d39de20cb9bb5
Leila Sol Mattone <leilarock@MacBook-Air-de-Leila.local>
2020-04-20 08:45:58 -0300

    Controler nueva competencia con todos los criterios

--- servidor/controladores/controladorNuevaCompetencia.js
+++ servidor/controladores/controladorNuevaCompetencia.js
@@ -4,12 +4,17 @@ module.exports = {
 
     postNuevaCompetencia: (req, res) => {
 
-        let nombre = req.body.nombre;
+        let nombreCompetencia = req.body.nombre;
+        let generoCompetencia = req.body.genero === '0' ? null : req.body.genero;
+        let directorCompetencia = req.body.director === '0' ? null : req.body.director;
+        let actorCompetencia = req.body.actor === '0' ? null : req.body.actor;
+
+        console.log(req.body);
+
     
 
-        connection.query(
-            'INSERT INTO competencias (nombre) VALUES (?)',
-            [nombre],
+        connection.query('INSERT INTO competencias (nombre, genero_id, director_id, actor_id) VALUES (?, ?, ?, ?) ',
+            [nombreCompetencia, generoCompetencia, directorCompetencia, actorCompetencia],
             (error, results, fields) => {
                 if (error) return console.error(error);
 
