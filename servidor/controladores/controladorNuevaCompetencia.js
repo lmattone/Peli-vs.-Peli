2517448a5869a12e39f4ea50311ba6a13d99ca8c
Leila Sol Mattone <leilarock@MacBook-Air-de-Leila.local>
2020-04-25 15:40:21 -0300

    Controladores

--- servidor/controladores/controladorNuevaCompetencia.js
+++ servidor/controladores/controladorNuevaCompetencia.js
@@ -8,6 +8,7 @@ module.exports = {
         let generoCompetencia = req.body.genero === '0' ? null : req.body.genero;
         let directorCompetencia = req.body.director === '0' ? null : req.body.director;
         let actorCompetencia = req.body.actor === '0' ? null : req.body.actor;
+        
 
         // Chequea la obligatoriedad del nombre de una competencia
         if (!nombreCompetencia)return res.status(400).send('Nombre es un campo obligatorio');
@@ -31,11 +32,11 @@ module.exports = {
 
         
         // Si elige una crear una competencia por por director, actor, genero o combinada
-        if (generoCompetencia && directorCompetencia && actorCompetencia) {
+        if (directorCompetencia && actorCompetencia && generoCompetencia) {
             sql +=  ' LEFT JOIN director_pelicula as Dp ON P.id = Dp.pelicula_id ' +
                     ' LEFT JOIN actor_pelicula as Ap ON P.id = Ap.pelicula_id ' +
                     ' WHERE Dp.director_id = ? AND Ap.actor_id = ? AND genero_id = ? ';
-            sqlParams.push(generoCompetencia, directorCompetencia, actorCompetencia);
+            sqlParams.push(directorCompetencia, actorCompetencia, generoCompetencia);
         } else if (directorCompetencia && actorCompetencia) {
             sql +=  ' LEFT JOIN director_pelicula as Dp ON P.id = Dp.pelicula_id ' +
                     ' LEFT JOIN actor_pelicula as Ap ON P.id = Ap.pelicula_id ' +
@@ -45,8 +46,8 @@ module.exports = {
             sql +=  ' LEFT JOIN director_pelicula as Dp ON P.id = Dp.pelicula_id WHERE Dp.director_id = ? AND genero_id = ? ';
             sqlParams.push(directorCompetencia, generoCompetencia);
         } else if (actorCompetencia && generoCompetencia) {
-            sql +=  ' LEFT JOIN actor_pelicula as Ap ON P.id = Ap.pelicula_id WHERE Ap.actor_id = ? ' +
-                    'AND genero_id = ? ';
+            sql +=  ' LEFT JOIN actor_pelicula as Ap ON P.id = Ap.pelicula_id' +
+                    ' WHERE Ap.actor_id = ? AND genero_id = ? ';
             sqlParams.push(actorCompetencia, generoCompetencia);
         } else if (generoCompetencia) {
             sql += ' WHERE genero_id = ? ';
@@ -61,8 +62,9 @@ module.exports = {
 
         console.log(sql);
         console.log(req.body);
+        console.log(sqlParams);
         connection.query(sql,
-            [sqlParams],
+            sqlParams,
             (error, results, fields) => {
                 if (error) return console.error(error);
                 if (results[0].cantidad_peliculas < 2) return res.status(400).json('No hay películas suficientes con esa combinación para la competencia');
